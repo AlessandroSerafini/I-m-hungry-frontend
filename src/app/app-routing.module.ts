f429d0079cf29eb74613074afebdecf92afdee48
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 17:32:51 +0100

    create login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {AddRestaurantComponent} from './components/pages/add-restaurant/add-rest
 import {UpdateRestaurantComponent} from './components/pages/update-restaurant/update-restaurant.component';
 import {MapPageComponent} from './components/pages/map-page/map-page.component';
 import {AuthGuard} from './guards/auth-guard.service';
+import {LoginPageComponent} from './components/pages/login-page/login-page.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path: 'add-restaurant',
     component: AddRestaurantComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
   },
   {
     path: 'update-restaurant/:id',
