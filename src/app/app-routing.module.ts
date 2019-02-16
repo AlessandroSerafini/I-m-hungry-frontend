695edaa970ba5367c756d8f9a27aab428421cd1e
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 12:13:49 +0100

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {ListPageComponent} from './components/pages/list-page/list-page.component';
+import {AddRestaurantComponent} from './components/pages/add-restaurant/add-restaurant.component';
+import {UpdateRestaurantComponent} from './components/pages/update-restaurant/update-restaurant.component';
 import {MapPageComponent} from './components/pages/map-page/map-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MapPageComponent
-  }
+  },
+  {
+    path: 'handle-restaurant',
+    component: ListPageComponent
+  },
+  {
+    path: 'add-restaurant',
+    component: AddRestaurantComponent
+  },
+  {
+    path: 'update-restaurant/:id',
+    component: UpdateRestaurantComponent
+  },
 ];
 
 @NgModule({
