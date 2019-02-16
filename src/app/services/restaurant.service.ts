44730b5472f49d0c1fd7e0dbe23cc5dfeea01596
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 10:56:31 +0100

    implemented getGooglePlace method in restaurant service

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -5,6 +5,7 @@ import {WebService} from './web.service';
   providedIn: 'root'
 })
 export class RestaurantService {
+  private googleApiKey = 'AIzaSyBtIQBtYgekv6YnUfXFGK3La0vm6armidQ';
   constructor(private webService: WebService) {
   }
 
@@ -20,6 +21,19 @@ export class RestaurantService {
     return new Promise((resolve, reject) => {
       this.webService.getJSON('/restaurants').then((restaurants) => {
         resolve(restaurants);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  public getGooglePlace(placeName: string = '', placeCity: string = ''): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' +
+        'json?input=' + encodeURIComponent(placeName) + ' ' + encodeURIComponent(placeCity) + '&inputtype=textquery' +
+        '&fields=place_id,photos&key=' + this.googleApiKey;
+      this.webService.getJSON(url, true).then((res) => {
+        resolve(res.candidates[0]);
       }).catch((err) => {
         reject(err);
       });
