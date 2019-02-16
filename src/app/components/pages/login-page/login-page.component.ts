5e7dee9350de5db00a99dae0a9dda2cf676519ef
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 17:49:32 +0100

    implements login

--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  model: any = {};
+  alert = {
+    visible: false,
+    type: '',
+    message: ''
+  };
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.alert = {
+      message: '',
+      type: '',
+      visible: false
+    };
+    this.authService.login(this.model.username, this.model.password).catch((err) => {
+      this.alert = {
+        message: err.error.message,
+        type: 'danger',
+        visible: true
+      };
+    });
   }
 
 }
