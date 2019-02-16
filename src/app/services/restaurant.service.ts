214c784e4c15322ab81f0dff94809e1a9703601f
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 10:55:28 +0100

    implemented getReview method in restaurant service

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -7,4 +7,12 @@ import {WebService} from './web.service';
 export class RestaurantService {
   constructor() {
   }
+
+  public getReview(restaurant): string {
+    let review = '';
+    if (typeof restaurant.reviews !== 'undefined' && restaurant.reviews.length > 0) {
+      review = restaurant.reviews[0].text + ' - ' + restaurant.reviews[0].text;
+    }
+    return review;
+  }
 }
