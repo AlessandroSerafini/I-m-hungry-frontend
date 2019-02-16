f8b4f994f24c44312e14a8815e6f19a962cc0828
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 11:48:26 +0100

    get restaurants in list page component

--- src/app/components/pages/list-page/list-page.component.ts
+++ src/app/components/pages/list-page/list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RestaurantService} from '../../../services/restaurant.service';
 
 @Component({
   selector: 'app-list-page',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-page.component.scss']
 })
 export class ListPageComponent implements OnInit {
+  private restaurants: Array<any> = [];
+  public isLoaded = false;
+  public filteredRestaurants: Array<any> = [];
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit() {
+    this.initList();
+  }
+
+  private initList() {
+    this.restaurants = [];
+    this.filteredRestaurants = [];
+    this.restaurantService.getRestaurants().then((restaurants) => {
+      restaurants.forEach((restaurant) => {
+        this.restaurantService.getGooglePlace(restaurant.name, restaurant.city).then((place) => {
+          if (typeof place !== 'undefined') {
+            this.restaurantService.getGooglePlaceDetails(place.place_id).then((placeDetails) => {
+              placeDetails.photoUrl = this.restaurantService.getGooglePlacePhoto(place.photos[0].photo_reference);
+              placeDetails.food = restaurant.food;
+              placeDetails.id = restaurant.id;
+              this.restaurants.push(placeDetails);
+            }).catch((err) => {
+              console.log(err);
+            });
+          }
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
 }
