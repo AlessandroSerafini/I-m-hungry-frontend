2282d0f3fcbfd3d6cea8a19f37c3d1f68faa78c7
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 10:52:52 +0100

    implemented getJSON method in web service

--- src/app/services/web.service.ts
+++ src/app/services/web.service.ts
@@ -5,7 +5,23 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class WebService {
+  private baseUrl = 'https://i-am-hungry.glitch.me';
 
   constructor(private http: HttpClient) {
+  }
+
+
+  public getJSON(path: string = '', isAbsolutePath: boolean = false): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const url = isAbsolutePath ? path : this.baseUrl + path;
+      this.http.get(url)
+        .toPromise()
+        .then((res) => {
+            resolve(res);
+          }
+        ).catch((err) => {
+        reject(err);
+      });
+    });
   }
 }
