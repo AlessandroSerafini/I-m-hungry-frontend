84aa51c71af52fadb742141caeb90df71824ea4d
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 11:16:22 +0100

    get restaurants in map page

--- src/app/components/pages/map-page/map-page.component.ts
+++ src/app/components/pages/map-page/map-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RestaurantService} from '../../../services/restaurant.service';
 
 @Component({
   selector: 'app-map-page',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-page.component.scss']
 })
 export class MapPageComponent implements OnInit {
+  private restaurants: Array<any> = [];
+  public filteredRestaurants: Array<any> = [];
 
-  constructor() { }
+  constructor(public restaurantService: RestaurantService) { }
 
   ngOnInit() {
+    this.restaurantService.getRestaurants().then((restaurants) => {
+      restaurants.forEach((restaurant) => {
+        this.restaurantService.getGooglePlace(restaurant.name, restaurant.city).then((place) => {
+          if (typeof place !== 'undefined') {
+            this.restaurantService.getGooglePlaceDetails(place.place_id).then((placeDetails) => {
+              placeDetails.photoUrl = this.restaurantService.getGooglePlacePhoto(place.photos[0].photo_reference);
+              placeDetails.food = restaurant.food;
+              placeDetails.id = restaurant.id;
+              this.restaurants.push(placeDetails);
+              this.filteredRestaurants.push(placeDetails);
+            }).catch((err) => {
+              console.log(err);
+            });
+          }
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
 }
