e146e40f19c75225d2ebf20cc76139c0f901e528
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 17:50:28 +0100

    handle redirect after login/logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,21 @@
 import {Injectable} from '@angular/core';
 import {WebService} from './web.service';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private webService: WebService) {
+  constructor(private webService: WebService,
+              private router: Router) {
   }
 
   public login(username: string = '', password: string = ''): Promise<any> {
     return new Promise((resolve, reject) => {
       this.webService.getJSON('/login?username=' + username + '&password=' + password).then((res) => {
         localStorage.setItem('isAuthenticated', 'true');
+        this.router.navigate(['/handle-restaurant']);
         resolve();
       }).catch((err) => {
         reject(err);
@@ -24,6 +27,7 @@ export class AuthService {
     return new Promise((resolve, reject) => {
       this.webService.getJSON('/logout').then((res) => {
         localStorage.removeItem('isAuthenticated');
+        this.router.navigate(['/login']);
         resolve();
       }).catch((err) => {
         reject(err);
