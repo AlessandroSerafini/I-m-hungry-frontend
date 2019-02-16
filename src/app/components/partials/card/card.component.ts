0afbcc0032f4e6a73ed5b0cb6ad07d58b68d8917
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 11:58:18 +0100

    implemented deleteRestaurant method in card component

--- src/app/components/partials/card/card.component.ts
+++ src/app/components/partials/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {RestaurantService} from '../../../services/restaurant.service';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() restaurant: any = null;
+  @Output() deleteRestaurantEmitter = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantService) {}
 
   ngOnInit() {
+  }
+
+  deleteRestaurant(restaurantId): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.restaurantService.deleteRestaurant(restaurantId)
+        .then((res) => {
+            this.deleteRestaurantEmitter.emit();
+            resolve(res);
+          }
+        ).catch((err) => {
+        reject(err);
+      });
+    });
   }
 
 }
