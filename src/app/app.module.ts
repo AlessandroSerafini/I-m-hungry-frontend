dae6bc5ca5ed2b97683b2955ff5bcf870e002e5e
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 12:07:30 +0100

    imported formmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/partials/header/header.component';
-import { MapPageComponent } from './components/pages/map-page/map-page.component';
-import { AgmCoreModule } from '@agm/core';
-import { RestaurantStarsComponent } from './components/partials/restaurant-stars/restaurant-stars.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/partials/header/header.component';
+import {MapPageComponent} from './components/pages/map-page/map-page.component';
+import {AgmCoreModule} from '@agm/core';
+import {FormsModule} from '@angular/forms';
+import {RestaurantStarsComponent} from './components/partials/restaurant-stars/restaurant-stars.component';
 import {SelectModule} from 'ng2-select';
 import {HttpClientModule} from '@angular/common/http';
-import { ListPageComponent } from './components/pages/list-page/list-page.component';
-import { CardComponent } from './components/partials/card/card.component';
-import { RestaurantFormComponent } from './components/partials/restaurant-form/restaurant-form.component';
+import {ListPageComponent} from './components/pages/list-page/list-page.component';
+import {CardComponent} from './components/partials/card/card.component';
+import {RestaurantFormComponent} from './components/partials/restaurant-form/restaurant-form.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { RestaurantFormComponent } from './components/partials/restaurant-form/r
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBtIQBtYgekv6YnUfXFGK3La0vm6armidQ'
@@ -35,4 +37,5 @@ import { RestaurantFormComponent } from './components/partials/restaurant-form/r
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
