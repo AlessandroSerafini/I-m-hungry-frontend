37866dc5e9e89f3ed22f2bc968900369d681f05c
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 12:02:57 +0100

    update form fields if form is in edit mode in restaurant form component

--- src/app/components/partials/restaurant-form/restaurant-form.component.ts
+++ src/app/components/partials/restaurant-form/restaurant-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {RestaurantService} from '../../../services/restaurant.service';
 
 @Component({
   selector: 'app-restaurant-form',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-form.component.scss']
 })
 export class RestaurantFormComponent implements OnInit {
+  isLoaded = false;
+  model: any = {};
+  foods: Array<any>;
+  @Input() restaurantId: string = null;
 
-  constructor() { }
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit() {
+    this.restaurantService.getFoods().then((foods) => {
+      this.foods = foods;
+      if (this.restaurantId !== null) {
+        this.restaurantService.getRestaurantDetail(this.restaurantId).then((restaurant) => {
+          this.model.name = restaurant.name;
+          this.model.city = restaurant.city;
+          this.model.food = restaurant.food;
+          this.isLoaded = true;
+        }).catch((err) => {
+          this.isLoaded = true;
+          console.log(err);
+        });
+      } else {
+        this.isLoaded = true;
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
 }
