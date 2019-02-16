c330870d4543b06dc0bf705af40964bd0f929a30
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 11:11:06 +0100

    created map page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { MapPageComponent } from './components/pages/map-page/map-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MapPageComponent
   ],
   imports: [
     BrowserModule,
