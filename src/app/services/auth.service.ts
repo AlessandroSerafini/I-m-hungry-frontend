4ae79c7c083231dcab384b6f8e63fcdf6b6c8023
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 17:29:17 +0100

    implemented login, logout and islogged methods in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {WebService} from './web.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private webService: WebService) {
+  }
+
+  public login(username: string = '', password: string = ''): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.webService.getJSON('/login?username=' + username + '&password=' + password).then((res) => {
+        localStorage.setItem('isAuthenticated', 'true');
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  public logout(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.webService.getJSON('/logout').then((res) => {
+        localStorage.removeItem('isAuthenticated');
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  public isLogged(): boolean {
+    return localStorage.getItem('isAuthenticated') !== null;
+  }
 }
