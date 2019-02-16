a316a63eba49437d9b3083bc1fbfd3cced20b05c
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 10:54:51 +0100

    created restaurant service

---
+++ src/app/services/restaurant.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+import {WebService} from './web.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+  constructor() {
+  }
+}
