a1923accd0b0b63d343d3081d3f55663d5d8abc7
Ale Serafini <aleserafini@MacBook-Pro-di-Ale.local>
2019-02-16 17:51:03 +0100

    added static login/logout links in header

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NavigationService} from '../../../services/navigation.service';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,8 @@ import {NavigationService} from '../../../services/navigation.service';
 export class HeaderComponent implements OnInit {
 
 
-  constructor(public navigationService: NavigationService) {
+  constructor(public navigationService: NavigationService,
+              public authService: AuthService) {
   }
 
   ngOnInit() {
